cb5ed04201fef0ba8fc3204cca8c9a88717a78ab
Anthony Sharp <anthonysharp15@gmail.com>
2021-01-16 18:38:06 +0000

    edited tweak rate calculation

--- Output.h
+++ Output.h
@@ -23,7 +23,7 @@ void PrintConsole() // Print a fresh console with all the bells and whistles
 {
 	system("CLS");
 
-	cout << "Simulation " << OurSimulation.CurrentNumberofSimulations << "/" << OurSimulation.GoalNumberofSimulations << ". Think " << currenthink << "/" << OurSimulation.ThinksPerSimulation << " (thinktime = " << thinktime << "s). Average Iteration " << (OurSimulation.CurrentAverageIteration+1) << "/" << OurSimulation.AverageAlgorithmTries << ". Max TweakChance = " << (1.0 * pow(0.90f, OurSimulation.BestNumberofPorkchops)) *100.0f << "%" << endl;
+	cout << "Simulation " << OurSimulation.CurrentNumberofSimulations << "/" << OurSimulation.GoalNumberofSimulations << ". Think " << currenthink << "/" << OurSimulation.ThinksPerSimulation << " (thinktime = " << thinktime << "s). Average Iteration " << (OurSimulation.CurrentAverageIteration+1) << "/" << OurSimulation.AverageAlgorithmTries << ". Max TweakChance = " << (1.0 * pow(0.86f, OurSimulation.BestNumberofPorkchops)) *100.0f << "%" << endl;
 	cout << "Layer1FilterSum: " << GetFilterWeightsSum(1) << endl;
 	cout << "Current simulation porkchops: " << OurSimulation.SimulationTotalPorkchops << +"(+" << HowMuchUncookedPork() << + ")" << ". Best number: " << OurSimulation.BestNumberofPorkchops << " (achieved at simulation " << OurSimulation.BestAchievedAtIteration << ")" << endl;
 	cout << "Progress debt: " << GetProgressDebt() << endl;
